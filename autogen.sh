05df6eb3d1b49bd1c0f255712dac5f60f0cd4c35
Benjamin Franzke <benjaminfranzke@googlemail.com>
2011-10-20 10:34:32 +0200

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-autoreconf --install
+#! /bin/sh
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+(
+  cd "$srcdir" &&
+  autoreconf --force -v --install
+) || exit
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
