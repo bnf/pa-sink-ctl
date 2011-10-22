68a08f272b6f4272bfb66f75361dd1102d54c1ad
Benjamin Franzke <benjaminfranzke@googlemail.com>
2011-10-22 10:02:11 +0200

    Add License header to source files

--- src/pa-sink-ctl.h
+++ src/pa-sink-ctl.h
@@ -1,3 +1,22 @@
+/*
+ * pa-sink-ctl - NCurses based Pulseaudio control client
+ * Copyright (C) 2011  Benjamin Franzke <benjaminfranzke@googlemail.com>
+ * Copyright (C) 2010  Jan Klemkow <web2p10@wemelug.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PA_SINK_CTL_H
 #define PA_SINK_CTL_H
 
