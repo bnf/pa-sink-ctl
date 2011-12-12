a76d55f553341b46c5b3bd4218df5aff3365433a
Benjamin Franzke <benjaminfranzke@googlemail.com>
2011-12-12 23:28:09 +0100

    Revert "Use g_list_find_custom to calculate sink position"
    This reverts commit 3898263f329e877d9b6a997d7a92851908d19166.

--- src/pa-sink-ctl.c
+++ src/pa-sink-ctl.c
@@ -114,13 +114,21 @@ get_sink_priority(struct context *ctx, const pa_sink_info *sink_info)
 	return 0;
 }
 
-static gint
-cmp_priority(gconstpointer list_data, gconstpointer b)
+static void
+add_sink(struct context *ctx, sink_info *new)
 {
-	const sink_info *sink_el = list_data;
-	const sink_info *sink = b;
+	GList *l, *pos = NULL;
 
-	return (sink->priority > sink_el->priority) ? 0 : -1;
+	for (l = ctx->sink_list; l; l = l->next) {
+		sink_info *sink = l->data;
+
+		if (new->priority > sink->priority) {
+			pos = l;
+			break;
+		}
+	}
+	ctx->sink_list = g_list_insert_before(ctx->sink_list, pos,
+					      g_memdup(new, sizeof *new));
 }
 
 static void
@@ -161,9 +169,7 @@ sink_info_cb(pa_context *c, const pa_sink_info *i,
 	if (inlist)
 		*inlist = sink;
 	else
-		list_insert_struct(ctx->sink_list, sink,
-				   g_list_find_custom(ctx->sink_list,
-						      &sink, cmp_priority));
+		add_sink(ctx, &sink);
 }
 
 static void
